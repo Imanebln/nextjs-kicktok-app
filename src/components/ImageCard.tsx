c9bf52dc68e6d183d84c30ac724c6ab2696c12bd
Imanebln <boulouane.imane@gmail.com>
2023-04-21 09:37:13 +0200

    added masonry css package to style images

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -5,25 +5,11 @@ import Logo from "../../utils/me4.jpg";
 import axios from "axios";
 const ImageCard = ({ image }: any) => {
   return (
-    <div>
-      <div className="flex gap-4 relative">
-        <div className="rounded-3xl">
-          {/* {imageData.map((image) => ( */}
-          <Link href="/">
-            <>
-              <Image
-                width={180}
-                height={300}
-                className="h-[100px] w-[50px] rounded-2xl cursor-pointer object-cover"
-                src={image.urls.regular}
-                alt="user-profile"
-              />
-            </>
-          </Link>
-          {/* ))} */}
-        </div>
-      </div>
-    </div>
+    <img
+      className="rounded-2xl cursor-pointer"
+      src={image.urls.regular}
+      alt={image.alt_description}
+    />
   );
 };
 
--- src/pages/images.tsx
+++ src/pages/images.tsx
@@ -1,16 +1,21 @@
 import ImageCard from "../components/ImageCard";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Masonry from "react-masonry-css";
 const Images = () => {
   const [imageData, setImageData] = useState([]);
 
   useEffect(() => {
+    // Generate a random number between 1 and 1000
+    const randomPage = Math.floor(Math.random() * 1000) + 1;
+
     // Make a request to the Unsplash API when the component mounts
     axios
-      .get("https://api.unsplash.com/photos", {
+      .get(`https://api.unsplash.com/photos?page=${randomPage}`, {
         params: {
           client_id: "WZvQi8paj4VtNEANcHjXbhyf6rviQZbtSRQV59kJoUk",
-          per_page: 50, // Number of images to retrieve
+          // query: "mercedes",
+          per_page: 30, // Number of images to retrieve
         },
       })
       .then((response) => {
@@ -20,12 +25,25 @@ const Images = () => {
         console.log(error);
       });
   }, []);
+
+  const breakpoints = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    486: 1,
+  };
   return (
-    <div className="flex flex-wrap gap-3 h-full">
-      {imageData.map((image) => (
-        <ImageCard image={image} />
+    <Masonry
+      breakpointCols={breakpoints}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {imageData.map((image, index) => (
+        <div key={index}>
+          <ImageCard image={image} />
+        </div>
       ))}
-    </div>
+    </Masonry>
   );
 };
 
